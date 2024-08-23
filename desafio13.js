4c103f6566b465a191b5d160dd467cc297003d93
biazinha45 <161720478+biazinha45@users.noreply.github.com>
2024-08-23 08:18:39 -0300

    Update desafio13.js

--- desafio13.js
+++ desafio13.js
@@ -15,3 +15,17 @@ function clique1() {
  function clique4() {
          jogahtml.innerHTML += '<p> Você clicou escolheu a primeira opção = botão 4</p>'
                        }
+
+function parimpar() {
+    let num = Number(window.prompt('Digite um número:'))
+    let tipo
+    if (num % 2 == 0) {
+        tipo = '<strong>PAR</strong>'
+    }else {
+        tipo = '<strong>ÍMPAR</strong>'
+    }
+
+
+    let res = document.querySelector('section#result')
+    res.innerHTML = `<p> O número digitado foi ${num} e ele é ${tipo}</p>`
+}
